1af6c5d0d0e3b8016550eb6d62a6e2f4db88831a
benoit.roblin <benoit.roblin@wonderbox.fr>
2018-08-06 14:03:19 +0200

    Ajout d'un TU pour le nouveau agen reflexe

---
+++ aima-core/src/test/java/aima/test/core/unit/environment/vacuum/PositionSensorSimpleReflexAgentTest.java
@@ -0,0 +1,117 @@
+package aima.test.core.unit.environment.vacuum;
+
+
+import aima.core.agent.impl.SimpleActionTracker;
+import aima.core.environment.vacuum.Coord;
+import aima.core.environment.vacuum.PositionSensorSimpleReflexAgent;
+import aima.core.environment.vacuum.VacuumEnvironment;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by benoit.roblin on 06/08/18.
+ */
+public class PositionSensorSimpleReflexAgentTest {
+
+    private PositionSensorSimpleReflexAgent agent;
+    private SimpleActionTracker actionTracker;
+
+    @Before
+    public void setUp() {
+        agent = new PositionSensorSimpleReflexAgent(2,1);
+        actionTracker = new SimpleActionTracker();
+    }
+
+    @Test
+    public void testCleanClean() {
+        VacuumEnvironment tve = new VacuumEnvironment(
+                VacuumEnvironment.LocationState.Clean,
+                VacuumEnvironment.LocationState.Clean);
+        tve.addAgent(agent, VacuumEnvironment.LOCATION_A);
+
+        tve.addEnvironmentView(actionTracker);
+
+        tve.step(8);
+
+        Assert.assertEquals(
+                "Action[name=Right], Action[name=Left], Action[name=Right], Action[name=Left], Action[name=Right], Action[name=Left], Action[name=Right], Action[name=Left]",
+                actionTracker.getActions());
+    }
+
+    @Test
+    public void testCleanDirty() {
+        VacuumEnvironment tve = new VacuumEnvironment(
+                VacuumEnvironment.LocationState.Clean,
+                VacuumEnvironment.LocationState.Dirty);
+        tve.addAgent(agent, VacuumEnvironment.LOCATION_A);
+
+        tve.addEnvironmentView(actionTracker);
+
+        tve.step(8);
+
+        Assert.assertEquals(
+                "Action[name=Right], Action[name=Suck], Action[name=Left], Action[name=Right], Action[name=Left], Action[name=Right], Action[name=Left], Action[name=Right]",
+                actionTracker.getActions());
+    }
+
+    @Test
+    public void testDirtyClean() {
+        VacuumEnvironment tve = new VacuumEnvironment(
+                VacuumEnvironment.LocationState.Dirty,
+                VacuumEnvironment.LocationState.Clean);
+        tve.addAgent(agent, VacuumEnvironment.LOCATION_A);
+
+        tve.addEnvironmentView(actionTracker);
+
+        tve.step(8);
+
+        Assert.assertEquals(
+                "Action[name=Suck], Action[name=Right], Action[name=Left], Action[name=Right], Action[name=Left], Action[name=Right], Action[name=Left], Action[name=Right]",
+                actionTracker.getActions());
+    }
+
+    @Test
+    public void testDirtyDirty() {
+        VacuumEnvironment tve = new VacuumEnvironment(
+                VacuumEnvironment.LocationState.Dirty,
+                VacuumEnvironment.LocationState.Dirty);
+        tve.addAgent(agent, VacuumEnvironment.LOCATION_A);
+
+        tve.addEnvironmentView(actionTracker);
+
+        tve.step(8);
+
+        Assert.assertEquals(
+                "Action[name=Suck], Action[name=Right], Action[name=Suck], Action[name=Left], Action[name=Right], Action[name=Left], Action[name=Right], Action[name=Left]",
+                actionTracker.getActions());
+    }
+
+    @Test
+    public void testCleanDirtyDirtyClean() {
+        VacuumEnvironment tve = new VacuumEnvironment(
+                new ArrayList<Coord>() {{
+                    add(new Coord(1,1));
+                    add(new Coord(1,2));
+                    add(new Coord(2,2));
+                    add(new Coord(2,1));
+                }},
+                new  VacuumEnvironment.LocationState[] {VacuumEnvironment.LocationState.Clean,
+                VacuumEnvironment.LocationState.Dirty,
+                VacuumEnvironment.LocationState.Dirty,
+                VacuumEnvironment.LocationState.Clean});
+        agent.configure(2,2);
+        tve.addAgent(agent, VacuumEnvironment.LOCATION_A);
+
+        tve.addEnvironmentView(actionTracker);
+
+        tve.step(8);
+
+        Assert.assertEquals(
+                "Action[name=Up], Action[name=Suck], Action[name=Right], Action[name=Suck], Action[name=Down], Action[name=Up], Action[name=Down], Action[name=Up]",
+                actionTracker.getActions());
+    }
+
+}
