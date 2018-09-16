62c3b7af274b14e4096f8cb5e303d0f4b876e18f
benoit <benoit_roblin@yahoo.fr>
2018-09-16 21:47:33 +0200

    chapitre 3 : 3.11 : implémentation du modèle

---
+++ aima-core/src/main/java/aima/core/environment/CannibalsAndMissionaries/CannibalsAndMissionariesModel.java
@@ -0,0 +1,162 @@
+package aima.core.environment.CannibalsAndMissionaries;
+
+/**
+ * TODO : methode indiquant que le but est atteint
+ *
+ * Created by benoit on 14/09/2018.
+ */
+public class CannibalsAndMissionariesModel {
+    int nbCannibalsInRightRiver=0;
+    int nbCannibalsInLeftRiver=3;
+    int nbCannibalsInBoat=0;
+
+    int nbMissionariesInRightRiver=0;
+    int nbMissionariesInLeftRiver=3;
+    int nbMissionariesInBoat=0;
+
+    boolean boatOnTheLeft = true;
+
+    public boolean isAForbiddenState(){
+        boolean cannibalMajorityOnRightRiver = nbMissionariesInRightRiver < nbCannibalsInRightRiver;
+        boolean cannibalMajorityOnLeftRiver = nbMissionariesInLeftRiver < nbCannibalsInLeftRiver;
+        boolean tooManyPersonsOnBoat = nbCannibalsInBoat + nbMissionariesInBoat > 2;
+        boolean negativeValue = nbCannibalsInRightRiver < 0 || nbCannibalsInLeftRiver < 0 || nbCannibalsInBoat <0 ||
+                nbMissionariesInRightRiver < 0 || nbMissionariesInLeftRiver < 0 || nbMissionariesInBoat <0;
+        return cannibalMajorityOnRightRiver || 
+                cannibalMajorityOnLeftRiver ||
+                tooManyPersonsOnBoat ||
+                negativeValue;
+    }
+    
+    public CannibalsAndMissionariesModel clone(){
+        CannibalsAndMissionariesModel model = new CannibalsAndMissionariesModel();
+        model.nbCannibalsInBoat = nbCannibalsInBoat;
+        model.nbCannibalsInRightRiver = nbCannibalsInRightRiver;
+        model.nbCannibalsInLeftRiver = nbCannibalsInLeftRiver;
+        return model;
+    }
+    
+    public CannibalsAndMissionariesModel generateNewModelFromAction(CannibalsAndMissionariesAction action){
+        CannibalsAndMissionariesModel newModel = this.clone();
+        switch (action){
+            case PUT_ONE_CANNIBAL_FROM_BOAT_TO_RIGHT_RIVER:
+                newModel.nbCannibalsInBoat--;
+                newModel.nbCannibalsInRightRiver++;
+                break;
+            case PUT_ONE_CANNIBAL_FROM_BOAT_TO_LEFT_RIVER:
+                newModel.nbCannibalsInBoat--;
+                newModel.nbCannibalsInLeftRiver++;
+                break;
+            case PUT_ONE_CANNIBAL_FROM_RIGHT_RIVER_TO_BOAT:
+                newModel.nbCannibalsInBoat++;
+                newModel.nbCannibalsInRightRiver--;
+                break;
+            case PUT_ONE_CANNIBAL_FROM_LEFT_RIVER_TO_BOAT:
+                newModel.nbCannibalsInBoat++;
+                newModel.nbCannibalsInLeftRiver--;
+                break;
+            case PUT_ONE_MISSIONARY_FROM_BOAT_TO_RIGHT_RIVER:
+                newModel.nbMissionariesInBoat--;
+                newModel.nbMissionariesInRightRiver++;
+                break;
+            case PUT_ONE_MISSIONARY_FROM_BOAT_TO_LEFT_RIVER:
+                newModel.nbMissionariesInBoat--;
+                newModel.nbMissionariesInLeftRiver++;
+                break;
+            case PUT_ONE_MISSIONARY_FROM_RIGHT_RIVER_TO_BOAT:
+                newModel.nbMissionariesInBoat++;
+                newModel.nbMissionariesInRightRiver--;
+                break;
+            case PUT_ONE_MISSIONARY_FROM_LEFT_RIVER_TO_BOAT:
+                newModel.nbMissionariesInBoat++;
+                newModel.nbMissionariesInLeftRiver--;
+                break;
+            default:
+                break;
+        }
+        return newModel;
+    }
+
+    public int getNbCannibalsInRightRiver() {
+        return nbCannibalsInRightRiver;
+    }
+
+    public void setNbCannibalsInRightRiver(int nbCannibalsInRightRiver) {
+        this.nbCannibalsInRightRiver = nbCannibalsInRightRiver;
+    }
+
+    public int getNbCannibalsInLeftRiver() {
+        return nbCannibalsInLeftRiver;
+    }
+
+    public void setNbCannibalsInLeftRiver(int nbCannibalsInLeftRiver) {
+        this.nbCannibalsInLeftRiver = nbCannibalsInLeftRiver;
+    }
+
+    public int getNbCannibalsInBoat() {
+        return nbCannibalsInBoat;
+    }
+
+    public void setNbCannibalsInBoat(int nbCannibalsInBoat) {
+        this.nbCannibalsInBoat = nbCannibalsInBoat;
+    }
+
+    public int getNbMissionariesInRightRiver() {
+        return nbMissionariesInRightRiver;
+    }
+
+    public void setNbMissionariesInRightRiver(int nbMissionariesInRightRiver) {
+        this.nbMissionariesInRightRiver = nbMissionariesInRightRiver;
+    }
+
+    public int getNbMissionariesInLeftRiver() {
+        return nbMissionariesInLeftRiver;
+    }
+
+    public void setNbMissionariesInLeftRiver(int nbMissionariesInLeftRiver) {
+        this.nbMissionariesInLeftRiver = nbMissionariesInLeftRiver;
+    }
+
+    public int getNbMissionariesInBoat() {
+        return nbMissionariesInBoat;
+    }
+
+    public void setNbMissionariesInBoat(int nbMissionariesInBoat) {
+        this.nbMissionariesInBoat = nbMissionariesInBoat;
+    }
+
+    public boolean isBoatOnTheLeft() {
+        return boatOnTheLeft;
+    }
+
+    public void setBoatOnTheLeft(boolean boatOnTheLeft) {
+        this.boatOnTheLeft = boatOnTheLeft;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CannibalsAndMissionariesModel that = (CannibalsAndMissionariesModel) o;
+
+        if (nbCannibalsInRightRiver != that.nbCannibalsInRightRiver) return false;
+        if (nbCannibalsInLeftRiver != that.nbCannibalsInLeftRiver) return false;
+        if (nbCannibalsInBoat != that.nbCannibalsInBoat) return false;
+        if (nbMissionariesInRightRiver != that.nbMissionariesInRightRiver) return false;
+        if (nbMissionariesInLeftRiver != that.nbMissionariesInLeftRiver) return false;
+        return nbMissionariesInBoat == that.nbMissionariesInBoat;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = nbCannibalsInRightRiver;
+        result = 31 * result + nbCannibalsInLeftRiver;
+        result = 31 * result + nbCannibalsInBoat;
+        result = 31 * result + nbMissionariesInRightRiver;
+        result = 31 * result + nbMissionariesInLeftRiver;
+        result = 31 * result + nbMissionariesInBoat;
+        return result;
+    }
+}
---
+++ aima-core/src/main/java/aima/core/environment/CannibalsAndMissionaries/ResolutionWithUniformCostAlgorithm.java
@@ -0,0 +1,15 @@
+package aima.core.environment.CannibalsAndMissionaries;
+
+/**
+ * Created by benoit on 14/09/2018.
+ */
+public class ResolutionWithUniformCostAlgorithm {
+    //TODO :
+    // faire une map avec les models en cours + coût
+    //faire une vairable avec une solution avec coût associé
+    //itérer sur totues les actions possibles
+    //filtrer les models avec états interdits.
+
+    //TODO :
+    //via un TU vérifier que le equals indique bien qu'un model est déjà présent.
+}
